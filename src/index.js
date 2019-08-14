2a3a1e82dc668533a792ab7db15a9bbd6f9b6e77
Boris Skert <boris.skert@adorsys.de>
2019-08-14 14:16:59 +0200

    Setting up the middleware

--- src/index.js
+++ src/index.js
@@ -52,3 +52,52 @@ app.post('/authenticate', (req, res) => {
     res.json({message: 'user not found !'})
   }
 })
+
+const ProtectedRoutes = express.Router();
+
+app.use('/api', ProtectedRoutes);
+
+ProtectedRoutes.use((req, res, next) => {
+
+  // check header for the token
+  var token = req.headers['access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks if the token is expired
+    jwt.verify(token, app.get('Secret'), (err, decoded) => {
+      if (err) {
+        return res.json({message: 'invalid token'});
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+
+    res.send({
+      message: 'No token provided.'
+    });
+
+  }
+});
+
+ProtectedRoutes.get('/getAllProducts', (req, res) => {
+  let products = [
+    {
+      id: 1,
+      name: 'cheese'
+    },
+    {
+      id: 2,
+      name: 'carottes'
+    }
+  ]
+
+  res.json(products)
+})
